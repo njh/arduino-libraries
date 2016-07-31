d05e9bbf47a20f96bd1fc46b776300a6f61ca157
Nicholas Humfrey <njh@aelius.com>
2016-07-31 16:51:33 +0100

    Made the multi-value lists prettier

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -3,6 +3,16 @@ def link_to(text, href=nil)
   "<a href='#{href}'>#{text}</a>"
 end
 
+def pretty_list(list)
+  if list.nil?
+    "<i>Unknown</i>"
+  elsif list == ['*']
+    "All"
+  else
+    list.join(', ')
+  end
+end
+
 def format_filesize(bytes)
   Filesize.new(bytes).pretty
 end
