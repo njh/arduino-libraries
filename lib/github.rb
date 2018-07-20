a2f6ed23ba6c62d0a8d3373da4a604b8357baf6d
Nicholas Humfrey <njh@aelius.com>
2018-07-20 13:10:35 +0100

    Implemented following redirects in the GitHub client

--- lib/github.rb
+++ lib/github.rb
@@ -3,11 +3,7 @@ require 'json'
 
 GITHUB_API = 'https://api.github.com/'
 
-def get_github(path, parameters={})
-  uri = Addressable::URI.parse(GITHUB_API)
-  uri.path = path
-  uri.query_values = parameters
-  
+def http_get_recursive(uri, count = 0)
   http = Net::HTTP.new(uri.host, uri.inferred_port)
   if uri.scheme == 'https'
     http.use_ssl = true
@@ -24,6 +20,24 @@ def get_github(path, parameters={})
   end
 
   response = http.request(request)
+  if response.code =~ /^3/
+    location = Addressable::URI.parse(response['Location'])
+    warn "Being redirected from '#{uri}' to '#{location}'"
+    raise "Redirected too many times" if count > 5
+    http_get_recursive(location, count + 1)
+  elsif response.code =~ /^2/
+    response
+  else
+    raise response
+  end
+end
+
+def get_github(path, parameters=nil)
+  uri = Addressable::URI.parse(GITHUB_API)
+  uri.path = path
+  uri.query_values = parameters unless parameters.nil?
+
+  response = http_get_recursive(uri)
   if response.content_type == 'application/json'
     JSON.parse(
       response.body,
